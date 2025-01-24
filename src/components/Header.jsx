a1c845eae5f3ebff23cdcf292d22f6fcfbb27831
Abolfazl <iabolfazl83@yahoo.com>
2025-01-24 13:32:20 +0330

    finished project.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logoimg from '../assets/quiz-logo.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logoimg} alt="Quiz Logo"/>
+      <h1>ReactQuiz</h1>
+    </header>
+  )
+}
